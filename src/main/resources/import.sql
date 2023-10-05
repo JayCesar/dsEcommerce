76fa6e97050c58820c8d76d18a5a106af8812495
JayCesar <batistajulio215@gmail.com>
2023-10-05 17:09:32 -0300

    erro no import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category(name) VALUES ('Livros');
+INSERT INTO tb_category(name) VALUES ('Eletronicos');
+
+INSERT INTO tb_product(name, description, price, img_url) VALUES ('Senhor dos aneis', 'Este livro é muito bom', 300.0, 'https://livros.com');
